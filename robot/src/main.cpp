705c5658e504b8ef97ccda00c876d17fea7028c1
liamkinne <kinneliam@gmail.com>
2018-04-01 18:00:52 +1000

    Test motor controller

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -1,8 +1,19 @@
-//#include <Oranje/TLE5206.h>
+#include <PWMOutput.h>
+#include <DigitalOutput.h>
+#include <L298.h>
+#include <Boards/ArduinoUno.h>
+#include <util/delay.h>
 
+using namespace Phantom;
 
+Oranje::L298 motor(new PWMOutput(new Pin(3)), new DigitalOutput(new Pin(2)), new DigitalOutput(new Pin(4)));
 
 int main(void)
 {
-
+	while (true) {
+		motor.set(0.75);
+		_delay_ms(1000);
+		motor.set(-0.75);
+		_delay_ms(1000);
+	}	
 }
